89cd942aaad5d813d029bb07e589a2cc4a3544a3
unknown <thongsopha123345@gmail.com>
2026-09-24 20:32:23 +0700

    add cell40

---
+++ discovery_piscine/cell40/ex4.1/age.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: "))
+
+for i in range(1, 4):
+    years = i * 10
+    print(f"In {years} years, you'll be {age + years} years old")
---
+++ discovery_piscine/cell40/ex4.2/calculator.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(str(num1) + " + " + str(num2) + " = ", num1 + num2)
+print(str(num1) + " - " + str(num2) + " = ", num1 - num2)
+print(str(num1) + " * " + str(num2) + " = ", num1 * num2)
+print(str(num1) + " / " + str(num2) + " = ", num1 / num2)
+
